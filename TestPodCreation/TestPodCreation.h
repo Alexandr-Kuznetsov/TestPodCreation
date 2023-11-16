c3fa6515afe9697f119e0c3a91a0f86d1d724d4f
Alex <alexandr_kuznetsov@icloud.com>
2023-11-16 13:50:59 +0100

    Initial setup

---
+++ TestPodCreation/TestPodCreation.h
@@ -0,0 +1,18 @@
+//
+//  TestPodCreation.h
+//  TestPodCreation
+//
+//  Created by Oleksandr Kuznietsov on 16/11/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TestPodCreation.
+FOUNDATION_EXPORT double TestPodCreationVersionNumber;
+
+//! Project version string for TestPodCreation.
+FOUNDATION_EXPORT const unsigned char TestPodCreationVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TestPodCreation/PublicHeader.h>
+
+
---
+++ TestPodCreationExample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestPodCreationExample
+//
+//  Created by Oleksandr Kuznietsov on 16/11/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
